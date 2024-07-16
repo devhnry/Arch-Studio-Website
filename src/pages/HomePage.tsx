0e214f57e9c9d251d2f75517f8587c55b76d6294
devhnry <devthenry@gmail.com>
2024-07-16 11:26:54 +0100

    feat: included carousel like property for the hero area

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
 import { Container } from "../components/Container";
 
 export const HomePage = () => {
+
   const links = ["portfolio", "about us", "contact"];
-  const screens = [1, 2, 3, 4];
+  const screens = [0, 1, 2, 3];
+  const [page, setPage] = useState(0);
+
+  const screenDetails = [
+    {
+      upperTitle: "Project",
+      lowerTitle: "Paramour",
+      details:
+        "Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.",
+    },
+    {
+      upperTitle: "Seraph",
+      lowerTitle: "Station",
+      details:
+        "The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.",
+    },
+    {
+      upperTitle: "Federal II",
+      lowerTitle: "Tower",
+      details:
+        "A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.",
+    },
+    {
+      upperTitle: "Trinity Bank",
+      lowerTitle: "Tower",
+      details:
+        "Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.",
+    },
+  ];
 
   return (
     <Container>
@@ -33,16 +63,17 @@ export const HomePage = () => {
       </nav>
 
       <section className="relative max-w-[570px] md:max-w-[1110px] mx-auto font-spartan">
-        <div className="bg-slate-600 bg-img min-h-[564px] w-full mx-auto h-[calc(100vh-136px)] max-h-[734px] text-white text-body md:text-[1.125rem] font-medium relative">
-          <div className="absolute bg-black opacity-[0.35] inset-0"></div>
+        <div
+          className={`bg-slate-600 bg-img bg-img-${page + 1} min-h-[564px] w-full mx-auto h-[calc(100vh-136px)] max-h-[734px] text-white text-body md:text-[1.125rem] font-medium relative`}
+        >
+          <div className="absolute bg-black opacity-[0.42] inset-0"></div>
           <div className="absolute left-[25%] -translate-x-[25%] p-6 grid gap-14 top-[46%] -translate-y-[46%]">
             <div className="grid gap-2">
-              <h1 className="leading-[clamp(3rem,3rem+3vw,5.1rem)] text-[clamp(3.2rem,3.2rem+6vw,6.1rem)] font-bold tracking-[0.075rem]">
-                Project <br /> Paramour
+              <h1 className="leading-[clamp(3rem,3rem+3vw,5.1rem)] text-[clamp(3.2rem,3.2rem+6vw,6.1rem)] font-bold tracking-[0.075rem] whitespace-nowrap">
+                {screenDetails[page].upperTitle} <br /> {screenDetails[page].lowerTitle}
               </h1>
-              <p className="max-w-[440px] min-w-[300px] pr-8">
-                Project made for an art museum near Southwest London. Project
-                Paramour is a statement of bold, modern architecture.
+              <p className="max-w-[450px] min-w-[300px] pr-8">
+                {screenDetails[page].details}
               </p>
             </div>
             <div className="flex gap-6 items-center bg-dark-blue w-fit px-7 py-5 cursor-pointer">
@@ -51,16 +82,17 @@ export const HomePage = () => {
             </div>
           </div>
           <div className="absolute left-[-49.24px] bottom-0 hidden xl:flex">
-            {screens.map((screen, index) => (
+            {screens.map((screen) => (
               <p
+                onClick={() => setPage(screen)}
                 className={`px-4 py-6 block items-center leading-[0] cursor-pointer hover:bg-dark-grey hover:text-white ${
-                  index === 0
+                  screen === page
                     ? "bg-dark-blue text-white"
                     : "text-dark-grey bg-white"
                 }`}
                 key={screen}
               >
-                0{screen}
+                0{screen + 1}
               </p>
             ))}
           </div>
