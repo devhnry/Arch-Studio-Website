4b988f655e7df8a7452f8549a1321a9625b42c3b
devhnry <devthenry@gmail.com>
2024-10-11 11:14:04 +0100

    feat(footercomponents): implemented footer componnent for both mobile and desktop views

--- src/components/FooterMobile.tsx
+++ src/components/FooterMobile.tsx
@@ -1,17 +1,20 @@
 import { Button } from "./Button";
+import React from "react";
 
-const links = ["portfolio", "about us", "contact"];
+export interface FooterProps {
+    links: string[]; // links is an array of strings
+}
 
-export const FooterMobile = () => {
+export const FooterMobile: React.FC<FooterProps> = ({ links }) => {
   return (
-    <div className="grid place-items-center items-end bg-thin-grey py-12 h-[380px] relative">
+    <div className="grid md:hidden place-items-center items-end bg-thin-grey py-12 h-[380px] relative md:px-24">
       <img
         className="bg-dark-blue p-4 size-24 absolute top-[-48px]"
         src="src/assets/icons/Arch-white.svg"
         alt=""
       />
       <div className="grid gap-8 capitalize text-dark-grey font-bold text-body text-[1.125rem]">
-        {links.map((link) => (
+        {links.map((link: string) => (
           <a key={link} href="">
             {link}
           </a>
