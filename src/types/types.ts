d2a5f8b0cede5bdf9021c3ab5a5c83a4c064016a
devhnry <devwhenry@gmail.com>
2024-10-13 18:09:20 +0100

    feat: created a types.ts to hold interfaces and export

---
+++ src/types/types.ts
@@ -0,0 +1,28 @@
+export interface FooterProps {
+	links: string[]; // links is an array of strings
+}
+
+export interface PortfolioCard {
+	title: string;
+	date: string;
+	url: string;
+}
+
+export interface screenDetails {
+	upperTitle: string;
+	lowerTitle: string;
+	details: string;
+	image: number;
+}
+
+export interface AboutCardDetails {
+	name: string;
+	title: string;
+	url: string;
+}
+
+export interface ContactInfoDetails {
+	mail: string;
+	address: string;
+	phone: string;
+}
