ecca2c433e49f641abd0028cb1ade5699ddefe96
devhnry <devthenry@gmail.com>
2024-10-13 18:12:45 +0100

    feat: completed contact page, contactInfo component to hold contact details

---
+++ src/components/ContactInfo.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {ContactInfoDetails} from "../types/types.ts";
+
+interface ContactInfoProps {
+	name: string;
+	info: ContactInfoDetails
+}
+
+export const ContactInfo: React.FC<ContactInfoProps> = ({ name, info}) => {
+	return (
+		<div className={`text-dark-grey w-full xsm:grid grid-cols-2 ls:grid-cols-1`}>
+			<div>
+				<h4 className={`font-bold text-[1.125rem] pb-4 capitalize`}>{name.split("-").map(n => (n + " "))}</h4>
+				{Object.entries(info).map(([key, value]) => (
+					<div key={key}>
+						<p className={`capitalize whitespace-nowrap`}>{key} :{`\t\t`} <span className={key === "address" ? `capitalize` : `lowercase`}>{value}</span></p>
+					</div>
+				))}
+			</div>
+				<a className={`flex flex-row ml-auto w-fit gap-2 items-center font-bold text-dark-blue pt-10 pl-1 leading-tight ls:ml-1 group cursor-pointer`}>
+					Link to Map
+					<img className={`group-hover:rotate-[30deg] transition-all duration-300`} src={`/src/assets/icons/icon-arrow.svg`} alt=""/>
+				</a>
+		</div>
+	);
+};
---
+++ src/pages/ContactUsPage.tsx
@@ -0,0 +1,62 @@
+import {Container} from "../components/Container.tsx";
+import Navbar from "../components/Navbar.tsx";
+import {links} from "../libs/constants.ts";
+import {HeaderArea} from "../components/HeaderArea.tsx";
+import useDisableScrollRestoration from "../hooks/useScrollRestoration.ts";
+import {Footer} from "../components/Footer.tsx";
+import {ContactInfo} from "../components/ContactInfo.tsx";
+import {ContactForm} from "../components/ContactForm.tsx";
+
+export const ContactUsPage = () => {
+
+	useDisableScrollRestoration()
+
+	const contactInfoOfficeDetails = {
+		"main-Office": {
+			mail: 'archone@mail.com',
+			address: '1892 Chenoweth Drive TN',
+			phone: '123-456-3451',
+		},
+		"office-II": {
+			mail: 'archtwo@mail.com',
+			address: '3399  Wines Lane TX',
+			phone: '832-123-4321',
+		}
+	}
+
+	return <>
+		<Container>
+			<Navbar links={links}/>
+			<HeaderArea page={`Contact`} url={`/src/assets/contact/image-header.png`}
+						title={`Tell us about your project`}
+						description={`We’d love to hear more about your project. Please, leave a message below or give us a call. We have two offices, one in Texas and one in Tennessee. If you find yourself nearby, come say hello!`}/>
+
+			{/*===== Contact Info ===== */}
+			<section className={`relative max-w-[570px] md:max-w-[1110px] mx-auto font-spartan px-5 my-20`}>
+				<div className={`w-[104px] bg-dark-grey opacity-50 h-[0.8px] ml-0`}></div>
+				<div className={`grid gap-10 ld:gap-14 ld:grid-cols-[250px_1fr] items-start`}>
+					<h2 className={`capitalize w-fit text-[clamp(3rem,9.14vw+1rem,4.2rem)] text-dark-blue leading-[1.05] font-[800] tracking-[0.075rem] pb-4 whitespace-nowrap mt-[4.125rem]`}>contact <br/> details
+					</h2>
+					<div className={`grid gap-8 ls:grid-cols-2 ls:w-fit ls:ml-auto ls:gap-20 ld:mt-[4.125rem]`}>
+						{Object.entries(contactInfoOfficeDetails).map(( [ key, info ] , index) => <ContactInfo key={index} info={info} name={key}/>)
+						}
+					</div>
+				</div>
+			</section>
+
+			{/*======== MAP ======= */}
+			<section className="relative max-w-[570px] md:max-w-[1110px] mx-auto font-spartan my-[72px] md:my-[120px] lg:my-[145px] xl:my-[190px">
+				<div id={`bg-map`} style={{backgroundImage: "url('/src/assets/contact/image-map.png')"}} className="bg-black bg-img h-[370px] transition-all duration-300 relative">
+				</div>
+			</section>
+
+			{/*=========== FORM ======== */}
+			<section className={`relative max-w-[570px] md:max-w-[1110px] mx-auto font-spartan px-5 my-20 lg:grid lg:grid-cols-[300px_1fr] lg:gap-20`}>
+				<h2 className={`capitalize w-fit text-[clamp(3rem,9.14vw+1rem,4.2rem)] text-dark-blue leading-[1.05] font-[800] tracking-[0.075rem] pb-4 whitespace-nowrap mt-[4.125rem]`}>Connect <br/> with us
+				</h2>
+				<ContactForm />
+			</section>
+			<Footer/>
+		</Container>
+	</>;
+};
