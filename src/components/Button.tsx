f9ae0d6330161282abc985b58d6118f2b4e4aeaa
devhnry <devwhenry@gmail.com>
2024-10-13 18:06:35 +0100

    refactor: updated styling of the button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,16 @@
 import { FC } from "react";
+import {Link} from "react-router-dom";
 
-interface Props{
+interface ButtonProps{
   text: string;
-  linkTo?: string
+  linkTo: string
 }
 
-
-export const Button: FC<Props> = ({text, linkTo}) => {
+export const Button: FC<ButtonProps> = ({text, linkTo}) => {
   return (
-    <div className="flex gap-6 items-center bg-dark-blue w-fit text-white px-7 py-5 cursor-pointer">
-      <a href={linkTo}>{text}</a>
-      <img src="src/assets/icons/icon-arrow-white.svg" alt="" />
-    </div>
+    <Link to={linkTo} className="flex group gap-6 items-center bg-dark-blue w-fit text-white px-7 py-5 cursor-pointer">
+      <p>{text}</p>
+      <img className={`group-hover:-rotate-[30deg] transition-all duration-500`} src="src/assets/icons/icon-arrow-white.svg" alt="" />
+    </Link>
   );
 }
