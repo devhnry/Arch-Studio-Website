eab7cbd49d7413d6d1f7cdfa49984c53f70b244b
devhnry <devwhenry@gmail.com>
2024-10-13 21:02:22 +0100

    refactor: image optimization

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -18,6 +18,12 @@ import thompsonSmith from "../assets/about/image-suit2.png";
 import jacksonRourke from "../assets/about/image-suit3.png";
 import mariaSimpson from "../assets/about/image-suit4.png";
 
+import bgImg1 from "../assets/home/image-8.png";
+import bgImg2 from "../assets/home/image-5.png";
+import bgImg3 from "../assets/home/image-6.png";
+import bgImg4 from "../assets/home/image-7.png";
+
+
 export const links = ["portfolio", "about us", "contact"];
 
 export const screenDetailsArray:screenDetails[] = [
@@ -26,28 +32,28 @@ export const screenDetailsArray:screenDetails[] = [
 		lowerTitle: "Paramour",
 		details:
 			"Project made for an art museum near Southwest London. Project Paramour is a statement of bold, modern architecture.",
-		image: 1,
+		image: bgImg1,
 	},
 	{
 		upperTitle: "Seraph",
 		lowerTitle: "Station",
 		details:
 			"The Seraph Station project challenged us to design a unique station that would transport people through time. The result is a fresh and futuristic model inspired by space stations.",
-		image: 2,
+		image: bgImg2,
 	},
 	{
 		upperTitle: "Federal II",
 		lowerTitle: "Tower",
 		details:
 			"A sequel theme project for a tower originally built in the 1800s. We achieved this with a striking look of brutal minimalism with modern touches.",
-		image: 3,
+		image: bgImg3,
 	},
 	{
 		upperTitle: "Trinity Bank",
 		lowerTitle: "Tower",
 		details:
 			"Trinity Bank challenged us to make a concept for a 84 story building located in the middle of a city with a high earthquake frequency. For this project we used curves to blend design and stability to meet our objectives.",
-		image: 4,
+		image: bgImg4,
 	},
 ];
 
