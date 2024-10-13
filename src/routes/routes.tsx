032fa96376ab46863263cd729d8e82e4d3c8a3fb
devhnry <devwhenry@gmail.com>
2024-10-13 18:04:33 +0100

    feat: updated routes to handle all pages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
+import PortfolioPage from "../pages/PortfolioPage.tsx";
+import {AboutUsPage} from "../pages/AboutPage.tsx";
+import {ContactUsPage} from "../pages/ContactUsPage.tsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage/>
   },
+  {
+    path: "/portfolio",
+    element: <PortfolioPage />
+  },
+  {
+    path: "/about-us",
+    element: <AboutUsPage />
+  },
+  {
+    path: "/contact",
+    element: <ContactUsPage />
+  }
 ]);
