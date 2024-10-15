a5bee9f07ad9654cda570546bdf3faaf85d112b0
devhnry <devthenry@gmail.com>
2024-10-15 16:58:12 +0100

    style: updated animation for button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,28 @@
 import { FC } from "react";
 import {Link} from "react-router-dom";
-
 import arrow from "../assets/icons/icon-arrow-white.svg"
+
+import {AnimatePresence, motion as m} from "framer-motion"
 
 interface ButtonProps{
   text: string;
-  linkTo: string
+  linkTo: string;
+  page?: number;
 }
 
-export const Button: FC<ButtonProps> = ({text, linkTo}) => {
+export const Button: FC<ButtonProps> = ({text, linkTo, page }) => {
   return (
-    <Link to={linkTo} className="flex group gap-6 items-center bg-dark-blue w-fit text-white px-7 py-5 cursor-pointer">
-      <p>{text}</p>
-      <img className={`group-hover:-rotate-[30deg] transition-all duration-500`} src={arrow} alt="" />
-    </Link>
+      <AnimatePresence>
+          <m.div key={page} initial={{opacity: 0, y: 0, x:0 }}
+                 animate={{opacity: 1, y:0,  transition: {
+                         duration: 0.5,
+                         delay: 0.5,
+                     }}}>
+            <Link to={linkTo} className="flex group gap-6 items-center bg-dark-blue w-fit text-white px-7 py-5 cursor-pointer">
+              <p>{text}</p>
+              <img className={`group-hover:-rotate-[30deg] transition-all duration-500`} src={arrow} alt="" />
+            </Link>
+          </m.div>
+      </AnimatePresence>
   );
 }
