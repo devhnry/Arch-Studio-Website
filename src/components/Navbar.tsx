ee618380661a7da97bf01261f3e6aa1fa72c7acd
devhnry <devthenry@gmail.com>
2025-01-20 22:24:10 +0100

    feat: updated the navbar link functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import {Link, useLocation} from "react-router-dom"
+import React, {useState} from "react";
+import { useLocation } from "react-router-dom"
 
 import { motion as m } from 'framer-motion';
 
 import logoBlack from "../assets/icons/Arch-black.svg"
 import hamburger from "../assets/icons/icon-hamburger.svg"
+import PageTransition from "./PageTransition.tsx";
 
 interface NavBarProps {
 	links: string[];
@@ -13,6 +14,11 @@ interface NavBarProps {
 const NavBar: React.FC<NavBarProps> = ({ links }) => {
 	const location = useLocation()
 	let pageLink: string;
+
+	const [changePage, setChangePage] = useState(false);
+	const [pageHeaded, setPageHeaded] = useState("");
+
+	const { pathname } = useLocation();
 
 	const container = {
 		hidden: { opacity: 1, scale: 0 },
@@ -37,7 +43,7 @@ const NavBar: React.FC<NavBarProps> = ({ links }) => {
 		}
 	};
 
-	switch (location.pathname) {
+	switch (pathname) {
 		case "/":
 			pageLink = "home"
 			break;
@@ -48,46 +54,62 @@ const NavBar: React.FC<NavBarProps> = ({ links }) => {
 			pageLink = location.pathname.split("/")[1];
 	}
 
+	const handleLinkClick = (link: string) => {
+		// If the current page is already the destination, set changePage to true and reset scroll
+		if (pageLink === link) {
+			setPageHeaded(link);
+			setChangePage(true);
+		} else {
+			setPageHeaded(link);
+			setChangePage(true);
+		}
+	};
+
 	return (
-		<nav className="p-6 md:py-12 flex justify-between md:justify-normal
+		<>
+			{changePage && <PageTransition pageHeaded={pageHeaded} changePage={changePage} setChangePage={setChangePage} /> }
+
+
+			<nav className="p-6 md:py-12 flex justify-between md:justify-normal
           gap-20 items-center max-w-[calc(570px+46px)] md:max-w-[1110px] md:px-0 mx-auto relative">
 
-			<div
-				className={`hidden absolute -top-[12px] -left-[50px] md:grid grid-rows-[minmax(104px,104px)_auto] items-start gap-y-8 opacity-50`}>
-				<div className={`w-[0.6px] bg-black h-[104px]`}></div>
-				<m.div variants={container} initial="hidden" animate="visible"  className={`grid gap-[2.5px] items-center`}>
-					{pageLink.split("").map((letter, index) => (
-						<m.p variants={item} initial={{ opacity: 0, scale: 0, rotate: 90 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} key={index + `${letter}`} className={`inline-block rotate-[90deg] uppercase text-[14px] pt-2`}>{letter}</m.p>
-					))}
-				</m.div>
-			</div>
-			<Link to={"/"}>
-				<m.img
-					initial={{scale: 0.75}}
-					animate={{scale: 1, transition: { duration: 1.4 }}}
-					className="block"
-					src={logoBlack}
-					alt="logo"
-				/>
-			</Link>
-			<>
-				<m.div variants={container} initial="hidden" animate="visible"  className="hidden md:flex gap-14 capitalize text-medium-grey text-body xl:text-[1.235rem] font-bold">
-					{links.map((link) => (
-						<m.div key={link} variants={item}>
-							<Link className="hover:text-dark-blue cursor-pointer transition-colors" key={link} to={`/${link == "about us" ? "about-us" : link}`} >
-								{link}
-							</Link>
+				<div
+					className={`hidden absolute -top-[12px] -left-[50px] md:grid grid-rows-[minmax(104px,104px)_auto] items-start gap-y-8 opacity-50`}>
+					<div className={`w-[0.6px] bg-black h-[104px]`}></div>
+					<m.div variants={container} initial="hidden" animate="visible" className={`grid gap-[2.5px] items-center`}>
+						{pageLink.split("").map((letter, index) => (
+							<m.p variants={item} initial={{opacity: 0, scale: 0, rotate: 90}} animate={{opacity: 1, scale: 1}} transition={{duration: 0.5}} key={index + `${letter}`} className={`inline-block rotate-[90deg] uppercase text-[14px] pt-2`}>{letter}</m.p>
+						))}
+					</m.div>
+				</div>
+				<p onClick={() => handleLinkClick("home")} >
+					<m.img
+						initial={{scale: 0.75}}
+						animate={{scale: 1, transition: {duration: 1.4}}}
+							className="block cursor-pointer"
+							src={logoBlack}
+							alt="logo"
+						/>
+					</p>
+					<>
+						<m.div variants={container} initial="hidden" animate="visible" className="hidden md:flex gap-14 capitalize text-medium-grey text-body xl:text-[1.235rem] font-bold">
+							{links.map((link) => (
+								<m.div key={link} variants={item}>
+									<p onClick={() => handleLinkClick(link)} className="hover:text-dark-blue cursor-pointer transition-colors" key={link}>
+										{link}
+									</p>
+								</m.div>
+							))}
 						</m.div>
-					))}
-				</m.div>
-				<img
-					className="md:hidden"
-					src={hamburger}
-					alt="menu"
-				/>
+						<img
+							className="md:hidden"
+							src={hamburger}
+							alt="menu"
+						/>
+					</>
+				</nav>
 			</>
-		</nav>
-	);
-};
+			);
+			};
 
-export default NavBar;
+			export default NavBar;
