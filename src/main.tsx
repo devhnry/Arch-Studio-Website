ebe7b66eed3b50f437d6e96bb8319a5c2683a2da
devhnry <devthenry@gmail.com>
2025-01-20 22:26:29 +0100

    feat: updated the vercel.json file to handle 'not found' error on page refresh

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/routes'
+import {AnimatePresence} from "framer-motion";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+      <AnimatePresence mode={`wait`}>
+        <RouterProvider router={router} />
+      </AnimatePresence>
   </React.StrictMode>,
 )
