a4fb36ba94ca45c45cb32cb08c432f5a2bd8deef
devhnry <devthenry@gmail.com>
2025-01-20 22:31:20 +0100

    feat: updated the about page with animations

--- src/components/AboutCardComponent.tsx
+++ src/components/AboutCardComponent.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { AboutCardDetails } from "../types/types.ts";
 
+import { motion as m } from "framer-motion";
+
 interface AboutCardDetailsExtended extends AboutCardDetails {
 	specialStyle: boolean;
+	index: number;
 }
 
-export const AboutCardComponent: React.FC<AboutCardDetailsExtended> = ({name, title, url, specialStyle}) => {
+export const AboutCardComponent: React.FC<AboutCardDetailsExtended> = ({name, title, url, specialStyle, index }) => {
 	return (
-		<div className={specialStyle ? `ml-auto` : ``}>
-			<img className={`pb-1 max-h-[350px] max-w-[310px] w-full`} src={`${url}`} alt={``}/>
+		<m.div initial={{x: (index + 2) % 2 == 0 ? -30 : 30, opacity: 0}} whileInView={{x: 0, opacity: 1, transition: {
+				duration: 0.5,
+				delay: 0.1
+			}}} viewport={{once : true}} className={specialStyle ? `ml-auto` : ``}>
+			<m.img className={`pb-1 max-h-[350px] max-w-[310px] w-full`} src={`${url}`} alt={``}/>
 			<h3 className={`text-[28px] ls:text-[22px] lg:text-[28px] transition-all duration-300 xl:text-[30px] font-[700] leading-tight ls:pb-[0.5px]`}>{name}</h3>
 			<p className={`text-body leading-tight`}>{title}</p>
-		</div>
+		</m.div>
 	);
 };
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -7,9 +7,13 @@ import {Footer} from "../components/Footer.tsx";
 import useDisableScrollRestoration from "../hooks/useScrollRestoration.ts";
 import heroPic from "../assets/about/image-header.webp"
 import bodyPic from "../assets/about/image-body.webp"
+import {motion as m} from "framer-motion";
 
 export const AboutUsPage = () => {
 	useDisableScrollRestoration()
+
+	const aboutText = ["Founded in 2007, we started as a trio of architects. Our complimentary skills and relentless attention to detail turned Arch into one of the most sought after boutique firms in the country.",  "Specialising in Urban Design allowed us to focus on creating exceptional structures that live in harmony with their surroundings. We consider every detail from every surrounding element to inform our designs.", "Our small team of world-class professionals provides input on every project."]
+
 	return (
 		<>
 			<Container>
@@ -24,23 +28,34 @@ export const AboutUsPage = () => {
 					<div className={`w-[104px] bg-black h-[0.8px] ml-[25px] md:ml-0`}></div>
 					<div className={`grid md:grid-cols-2 gap-20 mb-[4.125rem]`}>
 						<div className={`px-6 md:px-0`}>
-							<h2 className={`text-[clamp(3rem,9.14vw+1rem,4.2rem)] text-dark-blue leading-[1.05] font-[800] tracking-[0.075rem] pb-4 whitespace-nowrap mt-[4.125rem]`}>Our <br/> Heritage
-							</h2>
+							<m.h2 initial={{opacity: 0, scale: 0.8 }} viewport={{ once: true }} whileInView={{opacity: 1, y:0, scale:1,  transition: {
+								duration: 0.5,
+									delay: 0.1
+							}}} className={`text-[clamp(3rem,9.14vw+1rem,4.2rem)] text-dark-blue leading-[1.05] font-[800] tracking-[0.075rem] pb-4 whitespace-nowrap mt-[4.125rem]`}>Our <br/> Heritage
+							</m.h2>
 							<div className={`grid gap-5`}>
-								<p className={`text-body`}>Founded in 2007, we started as a trio of architects. Our complimentary skills and relentless attention to detail turned Arch into one of the most sought after boutique firms in the country.</p>
-								<p className={`text-body`}>Specialising in Urban Design allowed us to focus on creating exceptional structures that live in harmony with their surroundings. We consider every detail from every surrounding element to inform our designs.
-								</p>
-								<p className={`text-body`}>Our small team of world-class professionals provides input on every project.</p>
+								{aboutText.map((text, i) => (
+									<m.p className={`text-body`} initial={{opacity: 0, y:8 }} viewport={{ once: true }} whileInView={{opacity: 1, y:0,  transition: {
+										duration: 0.5,
+											delay: (i + 0.5) * 0.2
+									}}} key={i}>{text}</m.p>
+								))}
 							</div>
 						</div>
-						<div style={{backgroundImage: `url(${bodyPic})`}} className="max-w-[500px] ml-auto h-full w-full bg-img hidden md:block"></div>
+						<m.div initial={{opacity: 0, x:10 }} viewport={{ once: true }} whileInView={{opacity: 1, x:0, z:0,  transition: {
+							duration: 0.7,
+								delay: 0.4
+						}}}  style={{backgroundImage: `url(${bodyPic})`}} className="max-w-[500px] ml-auto h-full w-full bg-img hidden md:block"></m.div>
 					</div>
 				</section>
 
 				{/*========== THE LEADERS ====== */}
 				<section className={`relative max-w-[570px] mx-auto font-spartan md:max-w-[1110px] my-[120px] mb-[200px]`}>
 					<div className={`px-6 md:px-0 grid ls:grid-cols-[327px_1fr] ls:pt-[3.125rem]`}>
-						<h2 className={`text-[clamp(3rem,9.14vw+1rem,4.2rem)] text-dark-blue leading-[1] font-[800] tracking-[0.075rem] pb-6 whitespace-nowrap pt-[3.125rem] ls:pt-0`}>The<br/> Leaders</h2>
+						<m.h2 initial={{opacity: 0, x:-25 }} viewport={{ once: true }} whileInView={{opacity: 1, x:0, transition: {
+								duration: 0.5,
+								delay: 0.1
+							}}} className={`text-[clamp(3rem,9.14vw+1rem,4.2rem)] text-dark-blue leading-[1] font-[800] tracking-[0.075rem] pb-6 whitespace-nowrap pt-[3.125rem] ls:pt-0`}>The<br/> Leaders</m.h2>
 						<div className={`grid gap-y-10 xl:gap-y-14 gap-x-8 sm:grid-cols-2 max-w-[640px] w-full ml-auto`}>
 							{leaderInfo.map((info, index) => (
 								<AboutCardComponent
@@ -48,6 +63,7 @@ export const AboutUsPage = () => {
 									title={info.title}
 									specialStyle={index === 1 || index === 3}
 									url={info.url}
+									index={index}
 									key={index} />
 							))}
 						</div>
