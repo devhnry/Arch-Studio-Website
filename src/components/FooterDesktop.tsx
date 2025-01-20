ad9277f23d26234d790e711ae0928593be3084a3
devhnry <devthenry@gmail.com>
2025-01-20 22:24:41 +0100

    feat: updated the footer to adapt with the changes made to the navbar

--- src/components/FooterDesktop.tsx
+++ src/components/FooterDesktop.tsx
@@ -1,32 +1,45 @@
 import {Button} from "./Button.tsx";
-import React from "react";
-import {Link} from "react-router-dom";
+import React, {useState} from "react";
 import {FooterProps} from "../types/types.ts";
 import logoWhite from "../assets/icons/Arch-white.svg"
+import PageTransition from "./PageTransition.tsx";
 
 export const FooterDesktop: React.FC<FooterProps> = ({links}) => {
-	return <div className="relative hidden md:grid bg-thin-grey w-[calc(100%-110px)] md:max-w-[1110px] h-[120px]">
+
+	const [changePage, setChangePage] = useState(false);
+	const [pageHeaded, setPageHeaded] = useState("");
+
+	const handlePageChange = (link: string) => {
+		setPageHeaded(link);
+		setChangePage(true);
+	};
+
+	return <>
+		{changePage && <PageTransition pageHeaded={pageHeaded} changePage={changePage} setChangePage={setChangePage} /> }
+
+		<div className="relative hidden md:grid bg-thin-grey w-[calc(100%-110px)] md:max-w-[1110px] h-[120px]">
 		<div className={`flex flex-row gap-10 items-center`}>
-			<Link to={'/'}>
+			<div onClick={() => handlePageChange("home")} >
 				<p className={`sr-only`}>Home</p>
 				<img
-					className="bg-dark-blue p-4 size-[120px]"
+					className="bg-dark-blue p-4 size-[120px] cursor-pointer"
 					src={logoWhite}
 					alt=""
 				/>
-			</Link>
+			</div>
 			<ul className="flex gap-[52px] capitalize text-dark-grey font-bold text-[1.235rem]">
 				{links.map((link) => (
 					<li key={link}>
-						<Link to={`/${link == "about us" ? "about-us" : link}`} className={`inline-block`}>
+						<p onClick={() => handlePageChange(link)} className={`inline-block cursor-pointer`}>
 							{link}
-						</Link>
+						</p>
 					</li>
 				))}
 			</ul>
 		</div>
 		<div className={`absolute right-[-110px] top-[50%] -translate-y-[50%]`}>
-			<Button linkTo={"/portfolio"} text="See Our Portfolio"/>
+			<Button setChangePage={setChangePage} setPageHeaded={setPageHeaded}  linkTo={"portfolio"} text="See Our Portfolio"/>
 		</div>
-	</div>;
+	</div>
+		</>;
 };
--- src/components/FooterMobile.tsx
+++ src/components/FooterMobile.tsx
@@ -1,28 +1,41 @@
 import { Button } from "./Button";
-import React from "react";
-import {Link} from "react-router-dom";
+import React, {useState} from "react";
 import {FooterProps} from "../types/types.ts";
 import logoWhite from "../assets/icons/Arch-white.svg"
+import PageTransition from "./PageTransition.tsx";
 
 export const FooterMobile: React.FC<FooterProps> = ({ links }) => {
+
+    const [changePage, setChangePage] = useState(false);
+    const [pageHeaded, setPageHeaded] = useState("");
+
+    const handlePageChange = (link: string) => {
+        setPageHeaded(link);
+        setChangePage(true);
+    };
+
   return (
-    <div className="grid md:hidden place-items-center items-end bg-thin-grey py-12 h-[380px] relative md:px-24">
-        <Link to={"/"}>
-            <p className={`sr-only`}>Home</p>
-            <img
-                className="bg-dark-blue p-4 size-24 absolute top-[-48px] left-[50%] -translate-x-[50%]"
-                src={logoWhite}
-                alt=""
-            />
-        </Link>
-        <div className="grid gap-8 capitalize text-dark-grey font-bold text-body text-[1.125rem]">
-            {links.map((link: string) => (
-                <Link to={`/${link == "about us" ? "about-us" : link}`} key={link}>
-            {link}
-          </Link>
-        ))}
-      </div>
-      <Button linkTo={"/portfolio"} text="See Our Portfolio" />
-    </div>
+    <>
+        {changePage && <PageTransition pageHeaded={pageHeaded} changePage={changePage} setChangePage={setChangePage} /> }
+        <div className="grid md:hidden place-items-center items-end bg-thin-grey py-12 h-[380px] relative md:px-24">
+            <div onClick={() => handlePageChange("home")}>
+                <p className={`sr-only`}>Home</p>
+                <img
+                    className="bg-dark-blue p-4 size-24 absolute top-[-48px] left-[50%] -translate-x-[50%]"
+                    src={logoWhite}
+                    alt=""
+                />
+            </div>
+            <div className="grid gap-8 capitalize text-dark-grey font-bold text-body text-[1.125rem]">
+                {links.map((link: string) => (
+                    <p className={'cursor-pointer'} onClick={() => handlePageChange(link)}
+                        key={link}>
+                {link}
+              </p>
+            ))}
+          </div>
+            <Button setChangePage={setChangePage} setPageHeaded={setPageHeaded}  linkTo={"portfolio"} text="See Our Portfolio"/>
+        </div>
+    </>
   );
 };
