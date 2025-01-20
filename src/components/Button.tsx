93af64ce7a91c20ae3a9fcb07adbe439470ab0f5
devhnry <devthenry@gmail.com>
2025-01-20 22:25:54 +0100

    feat: updated the button component to handle navigation differently

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 import arrow from "../assets/icons/icon-arrow-white.svg"
 
 import {AnimatePresence, motion as m} from "framer-motion"
@@ -8,20 +8,30 @@ interface ButtonProps{
   text: string;
   linkTo: string;
   page?: number;
+  setChangePage: (changePage: boolean) => void;
+  setPageHeaded: (pageHeaded: string) => void;
+  specialStyle? : boolean;
 }
 
-export const Button: FC<ButtonProps> = ({text, linkTo, page }) => {
+export const Button: FC<ButtonProps> = ({text, linkTo, page, setChangePage, setPageHeaded, specialStyle }) => {
+
+    const handleClick = () => {
+        setPageHeaded(linkTo == "about us" ? "about-us" : linkTo); // Set the target page
+        setChangePage(true);   // Trigger page transition
+    };
+
   return (
       <AnimatePresence>
           <m.div key={page} initial={{opacity: 0, y: 0, x:0 }}
                  animate={{opacity: 1, y:0,  transition: {
                          duration: 0.5,
                          delay: 0.5,
-                     }}}>
-            <Link to={linkTo} className="flex group gap-6 items-center bg-dark-blue w-fit text-white px-7 py-5 cursor-pointer">
+                     }}}
+                 onClick={handleClick}>
+            <div className={`flex group gap-6 items-center bg-dark-blue ${specialStyle ? "w-full place-content-center" : "w-fit"} text-white px-7 py-5 cursor-pointer`}>
               <p>{text}</p>
               <img className={`group-hover:-rotate-[30deg] transition-all duration-500`} src={arrow} alt="" />
-            </Link>
+            </div>
           </m.div>
       </AnimatePresence>
   );
